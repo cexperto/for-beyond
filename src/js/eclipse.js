c4511cf66a3ebf7780e3d30f37ec262d7ca0d08d
cexperto <rattman.c@gmail.com>
2021-02-24 20:06:49 -0500

    Panel eclipse

---
+++ src/js/eclipse.js
@@ -0,0 +1,20 @@
+function eclipse(){
+        navegacion = document.getElementById('item-hidden')
+        //console.log(navegacion);
+
+        if(navegacion.style.display == 'block'){
+                navegacion.style.display = 'none';
+        }else{
+                navegacion.style.display = 'block';
+        }
+}
+function eclipsev2(){
+        navegacion2 = document.getElementById('item-hidden2')
+        //console.log(navegacion);
+        
+        if(navegacion2.style.display == 'block'){
+            navegacion2.style.display = 'none';
+        }else{
+            navegacion2.style.display = 'block';
+        }
+    }
---
+++ src/js/panel.js
@@ -0,0 +1,10 @@
+function btnmenu(){
+    navegacion = document.getElementById('menubox')
+    //console.log(navegacion);
+    
+    if(navegacion.style.display == 'block'){
+        navegacion.style.display = 'none';
+    }else{
+        navegacion.style.display = 'block';
+    }
+}
